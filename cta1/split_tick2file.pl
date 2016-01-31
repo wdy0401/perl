b12f5d7fde64d92a4c5ce97fa78e039ef7c1053c
wdy0401 <wdy0401@gmail.com>
2016-01-31 15:33:42 +0800

    split tick file

---
+++ cta1/split_tick2file.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use File::Path;
+opendir(DH,"./tick") or die "cannot open dir ./nd\n";
+for my $file(readdir DH)
+{
+	next unless $file=~/csv/;
+	&split_file($file);
+}
+sub split_file(@)
+{
+	my $f=shift @_;
+	my %dh=();
+	open(IN,"./tick/$f") or die "Cannot open file $f\n";
+	while(<IN>)
+	{
+		my $d=(split/,/)[0];
+		if(!defined $dh{$d})
+		{
+			my @a;
+			$dh{$d}=\@a;
+		}
+		push @{$dh{$d}},"$_";
+	}
+	close IN;
+	for my $date(sort keys %dh)
+	{
+		mkpath "./split_tick/$date";
+		open(OUT," >./split_tick/$date/$f") or die"Cannot open file ./split_tick/$date/$f\n";
+		print OUT join"",@{$dh{$date}};
+		close OUT;
+	}
+}
